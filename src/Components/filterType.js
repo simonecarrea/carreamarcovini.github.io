95e5d1e7c50df0392d0d7930ab817bf9f0a6a447
Simone Carrea <carre.7@hotmail.it>
2025-01-22 16:43:03 +0100

    Updates

--- src/Components/filterType.js
+++
@@ -1,9 +0,0 @@
-// FiltriTypes.ts
-
-const Filtri = {
-    filtroTipologia: string,
-    filtroTipologia: string,
-  };
-  
-  export default Filtri;
-  
--- src/services/contentful.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'contentful';
-
-const client = createClient({
-  space: 'u302zor4y2qg',
-  accessToken: 'Q7Lpsf-u_AjsJ-FroNo3B1Dh35dEAU0j5g2NQ2eTTXs',
-});
-
-export default client;
